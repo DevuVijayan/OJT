f10828ba0196855556f5e5ce2e636fb4f7cf25cc
DevuVijayan <122440674+DevuVijayan@users.noreply.github.com>
2024-05-17 14:36:14 +0530

    Delete Day2 directory

--- Day2/iterate.py
+++
@@ -1,5 +0,0 @@
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-
-# Iterate over keys and values
-for key, value in my_dict.items():
-    print(f'Key: {key}, Value: {value}')
--- Day2/rmv_element.py
+++
@@ -1,7 +0,0 @@
-# Sample list
-my_list = [1, 2, 3, 4, 5]
-
-# Remove element with value 3
-my_list.remove(3)
-
-print(my_list)  # Output: [1, 2, 4, 5]
