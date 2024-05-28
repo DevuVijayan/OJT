1619e4682245d32ff470a0ddc9a255d306c40840
DevuVijayan <122440674+DevuVijayan@users.noreply.github.com>
2024-05-28 14:24:46 +0530

    Add files via upload

---
+++ Day8/Standard.py
@@ -0,0 +1,10 @@
+# Importing necessary libraries
+import numpy as np
+
+# Sample data
+data = [1, 2, 3, 4, 5]
+
+# Calculating the standard deviation
+std_dev = np.std(data)
+
+print("Standard Deviation:", std_dev)
